54547b1e86069d3457f34ec864ee7d376d1ac8d1
Jackson Crawford <72364545+jjagn@users.noreply.github.com>
2024-02-14 13:52:00 +1300

    initial commit

---
+++ init.lua
@@ -0,0 +1,49 @@
+local impatient_ok, impatient = pcall(require, "impatient")
+if impatient_ok then impatient.enable_profile() end
+
+for _, source in ipairs {
+  "core.utils",
+  "core.options",
+  "core.bootstrap",
+  "core.diagnostics",
+  "core.autocmds",
+  "core.mappings",
+  "configs.which-key-register",
+} do
+  local status_ok, fault = pcall(require, source)
+  if not status_ok then vim.api.nvim_err_writeln("Failed to load " .. source .. "\n\n" .. fault) end
+end
+
+astronvim.conditional_func(astronvim.user_plugin_opts("polish", nil, false))
+
+vim.o.guifont = "Berkeley_Mono:h10"
+
+vim.g.neovide_refresh_rate = 165
+
+if vim.fn.has "nvim-0.8" ~= 1 or vim.version().prerelease then
+  vim.schedule(function() astronvim.notify("Unsupported Neovim Version! Please check the requirements", "error") end)
+end
+
+
+-- init.lua
+
+-- Load custom treesitter grammar for org filetype
+require('orgmode').setup_ts_grammar()
+
+-- Treesitter configuration
+require('nvim-treesitter.configs').setup {
+  -- If TS highlights are not enabled at all, or disabled via `disable` prop,
+  -- highlighting will fallback to default Vim syntax highlighting
+  highlight = {
+    enable = true,
+    -- Required for spellcheck, some LaTex highlights and
+    -- code block highlights that do not have ts grammar
+    additional_vim_regex_highlighting = {'org'},
+  },
+  ensure_installed = {'org'}, -- Or run :TSUpdate org
+}
+
+--require('orgmode').setup({
+--   org_agenda_files = "C:\Users\Jackson\Desktop\stuff\orgmode\*",
+--   org_default_notes_file = "C:\Users\Jackson\Desktop\stuff\orgmode\refile.org",
+--})
